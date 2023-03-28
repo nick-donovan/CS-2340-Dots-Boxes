ae5509eec2289be94fb351fab0d24f6ecb217e33
Nick Donovan <114261268+nick-donovan@users.noreply.github.com>
2023-03-28 17:03:49 -0500

    refactor(main.asm): replaced tabs with spaces because it's annoying

--- main.asm
+++ main.asm
@@ -1,5 +1,5 @@
 # File: main.asm
-# Author: [ADD YOUR NAMES HERE IF YOU WORK ON IT]
+# Author: Nicholas Donovan
 # Class: CS 2340.004
 # Date: Mar. 26, 2023
 # Purpose: Responsible for tying everything together and running the game loop. Functions 
@@ -8,17 +8,17 @@
 
 .text
 main:
-	addi $sp, $sp, -4
-	sw $ra, 0($sp)
-	
-	jal initializeBoard
-	jal printBoard
-	
-	lw $ra, 0($sp)
-	addi $sp, $sp, 4
-	
-	j exit
-	  
+        addi $sp, $sp, -4
+        sw $ra, 0($sp)
+        
+        jal initializeBoard
+        jal printBoard
+        
+        lw $ra, 0($sp)
+        addi $sp, $sp, 4
+        
+        j exit
+          
 exit:
         li $v0, 10                   # Syscall for program term
         syscall                      # Exit program
