54d8c59909d54d5d25bc96cc2d5e96567677a6d5
Nick Donovan <114261268+nick-donovan@users.noreply.github.com>
2023-04-02 21:58:06 -0500

    refactor(board.asm): adding header, pseudo, and rewriting initBoard for easier readability

--- board.asm
+++ board.asm
@@ -22,67 +22,93 @@
         
 .text
 main:
+	jal initializeBoard
+	jal printBoard
+
         li $v0, 10
         syscall
 
+# Description: Initializes the game board with the boardSymbolChar as dots 
+#              and spaces for empty areas.
+#
+# Pseudo representation:
+#     public void initializeBoard():
+#         for(int row = 0; row < boardRowSize; ++row):
+#         	for (int col = 0; col < boardColumnSize; ++col):
+#         	    if (row.isEven() && col.isEven()):
+#         	        boardArray[row][col] = boardSymbolChar
+#         	    else
+#         	        boardArray[row][col] = ' '
+#         	end for
+#         end for
+#     end initializeBoard
+#
+# Inputs: 
+#   None
+# Outputs:
+#   None - Initialized board is saved in memory
+# Registers modified: None
 initializeBoard:
-        addi $sp, $sp, -4        # Make room in stack
-        sw $ra, 0($sp)           # Store return address in stack
+        addi $sp, $sp, -4
+        sw $ra, 0($sp)
 
         la $t0, boardArray       # Load address of the array into $t0
         la $t1, boardRowSize     # Load rowSize address into $t1
         lb $t1, ($t1)            # Set $t1 to rowSize integer
         la $t2, boardColumnSize  # Load colSize address into $t2
-        lb $t2, ($t2)            # Set $t2 to colSize integer
-        li $t3, 0                # Initialize row index to 0
-        li $t4, 0                # Initialize col index to 0 (not needed here but helps know which reg it's in)
+        lb $t2, ($t2)            # Set $t2 to colSize integer 
+        li $t3, 0                # Initialize row index to 0 (int row = 0)
         
+        # Loop through each row of the game board
+        iBRowLoop:
+                slt $t5, $t3, $t1      # Is row < boardRowSize
+                beq $t5, $zero, iBExit # If not, exit
+                
+                li $t4, 0              # Set col index to 0 (int col = 0)
+
+                # Loop through each column of the game board
+                iBColLoop:
+                        slt $t5, $t4, $t2            # Is col < boardColumnSize
+                        beq $t5, $zero, iBColLoopEnd # If not, move to next row
+
+                        # Check if current position is even
+                        andi $t5, $t3, 0x01          # Use AND to check if row index is even
+                        bnez $t5, loadSpace          # Jump to loadSpace if row index is odd
+
+                        andi $t5, $t4, 0x01          # Use AND to check if col index is even
+                        bnez $t5, loadSpace          # Jump to loadSpace if col index is odd
+
+                        lb $t6, boardSymbolChar      # Load boardSymbolChar into $t6
+
+                        j iBInsertChar               # Insert into the game board array
+ 
+                        
+                        loadSpace:
+                                lb $t6, spaceChar    # Load the space character into $t6
+
+                        # Insert the loaded symbol into the game board array
+                        iBInsertChar:
+                                mul $t5, $t3, $t2    # Calculate the index of the current position on the game board array
+                                add $t5, $t5, $t4
+                                add $t5, $t5, $t0    # Add the address of the game board to the index
+
+                                sb $t6, ($t5)        # Insert the loaded char into the game board array
+
+                        addi $t4, $t4, 1             # Increment col index
+
+                        j iBColLoop                  # Move to next element
+
+                # End of the column loop
+                iBColLoopEnd:
+                        addi $t3, $t3, 1             # Increment row index
+                        j iBRowLoop                  # Move to next row
+
+        iBExit:
+                lw $ra, 0($sp)                       # Load return address from stack
+                addi $sp, $sp, 4                     # Free up stack space
+
+                jr $ra                               # Return
         
-        initializeBoardRowLoop:
-                blt $t3, $t1, initializeBoardColumnLoopStart  # If row index is less than the amount of rows, loop column 
-                j initializeBoardExit                         # Else, exit the loop
-
-                initializeBoardColumnLoopStart:
-                        li $t4, 0                                     # Reset column index to 0
-                        
-                initializeBoardColumnLoop:
-                        blt $t4, $t2, initializeBoardChar             # Jump if column index is less that the total columns in a row
-                        j initializeBoardColumnLoopEnd                # Processed to next row
-                        
-                initializeBoardChar:
-                        mul $t5, $t3, $t2           # Multiply the row we're on by total columns
-                        add $t5, $t5, $t4           # Add column index to $t5 to get current element index
-                        add $t5, $t0, $t5           # Set $t5 to current cell address in array
-                        
-                        andi $t6, $t3, 0x01         # Check if row index is even
-                        bnez $t6, loadSpace         # If odd, insert a space in the cell
-                        
-                        andi $t6, $t4, 0x01         # Check if the column index is even
-                        bnez $t6, loadSpace         # If odd, insert a space in the cell
-                        
-                        addi $t4, $t4, 1            # Increment counter
-                        
-                        lb $t6, boardSymbolChar     # Load boardSymbolChar in $t6 for insertion
-                        j initializeBoardAddChar    # Jump to label that inserts char in array
-                        
-                loadSpace:
-                        addi $t4, $t4, 1            # Increment column index
-                        lb $t6, spaceChar           # Load space into $t6 for insertion
-                        
-                initializeBoardAddChar:
-                        sb $t6, ($t5)               # Store the character in the array
-                        j initializeBoardColumnLoop # Process next cell in row
-                
-        initializeBoardColumnLoopEnd:
-                addi $t3, $t3, 1            # Increment row index
-                j initializeBoardRowLoop    # Process next row in array
-                
-initializeBoardExit:
-        lw $ra, 0($sp)              # Load return address from stack
-        addi $sp, $sp, 4            # Restore the stack
-
-        jr $ra                      # Jump to return address
-
 printBoard:
         addi $sp, $sp, -4           # Make room in stack
         sw $ra, 0($sp)              # Store return address in stack
