d4181e781e70b9313c753d73880fe89bd75ee147
Nick Donovan <114261268+nick-donovan@users.noreply.github.com>
2023-04-04 23:57:21 -0500

    refactor(input.asm): finishing some input methods, adding some method headers with pseudo

--- input.asm
+++ input.asm
@@ -9,153 +9,173 @@
 .data
         userSelectedEdge: .space 3
         userSelectedEdgeSize: .byte 4
+        edgeStringMaxLen: .byte 3
         userPromptString: .asciiz "Enter an edge to claim (ex: A2): "
         invalidInputWarning: .asciiz "Please enter a letter (A-O) and a number (0-11)."
-
-        
+        insfCharactersWarning: .asciiz "PLACEHOLDER WARNING - NOT ENOUGH CHARS IN EDGE STRING"
+        .globl getUserInput
+        .globl convertEdgeString
 
 .text
 main:
-        j getUserInput
 
+        jal getUserInput
+        j exit
+
+# Description: Prints the userPromptString and saves a selected edge into 
+#              userSelectedEdge before having it converted to two indicies.
+#
+# Pseudo representation:
+#     public (int, int) getUserInput():
+#         print(userPromptString)
+#         a0 = input.next
+#         (col, row) = convertEdgeString(a0)
+#         return (col, row)
+#     end getUserInput()
+# Inputs: 
+#   None
+# Outputs:
+#   $v0 - the col index of the selected edge
+#   $v1 - the row index of the selected edge
+# Registers modified: $sp, $ra, $a0
 getUserInput:
-        addi $sp, $sp -16
+        addi $sp, $sp -4
         sw $ra, 0($sp)
-        sw $a0, 4($sp)
-        sw $a1, 8($sp)
-        sw $a2, 12($sp)
         
         la $a0, userPromptString
-        li $v0, 4
-        syscall
+        jal printString
         
         la $a0, userSelectedEdge
         lb $a1, userSelectedEdgeSize
         li $v0, 8
         syscall
         
+        jal convertEdgeString
+        
+        # todo: jal validateUserInput
+        
+        lw $ra, 0($sp)
+        addi $sp, $sp, 4
+        
+        jr $ra
+        
+convertEdgeString:
+        addi $sp, $sp -4
+        sw $ra, 0($sp)
+      
         jal getLength
+
+        # todo: add error handling for len < 2
         
         move $a1, $v0
-        
-        # Input may be A0-O11
         jal splitInput
 
-        move $t0, $v0
-        move $t1, $v1
-
-        li $a0, '\n'
-        li $v0, 11
-        syscall
-                        
-        move $a0, $t0
-        li $v0, 1
-        syscall
-        
-        li $a0, '\n'
-        li $v0, 11
-        syscall
-        
-        move $a0, $t1
-        li $v0, 1
-        syscall
-        
-        j exit
-        
-        # jal validateUserInput TODO
-        
-        subi $v0, $a1, 0x41 # Convert to column index
-        subi $v1, $a2, 0x30 # Convert to row index
-        
-        move $a0, $v0
-        li $v0, 1
-        syscall
-        
-        move $a0, $v1
-        li $v0, 1
-        syscall
-        
-#        li $a0, 2222
-#        li $v0, 1
-#        syscall
-        
-        lw $a2, 12($sp)
-        lw $a1, 8($sp)
-        lw $a0, 4($sp)
         lw $ra, 0($sp)
-        addi $sp, $sp, 16
+        addi $sp, $sp, 4
         
-        j exit
-
-getLength:
-        move $t0, $a0 # First index of input string
-        li $t1, 0   # Input string length
-        
-getLengthLoop:
-        lb $t2, ($t0)
-        beqz $t2, getLengthEnd # If null terminated
-        seq $t2, $t2, 0xa
-        bnez $t2, getLengthEnd # If line feed terminated
-        
-        
-        add $t0, $t0, 1
-        addi $t1, $t1, 1
-        
-        j getLengthLoop
-        
-getLengthEnd:
-        move $v0, $t1
         jr $ra
 
+# Description: Returns the length of a string
+#
+# Pseudo representation:
+#     private int getLength(String a0):
+#         int len = 0
+#         for (char in a0):
+#             if (len >= edgeStringMaxLen || char == 0x00 || char == 0x0A):
+#                 break
+#             ++len
+#         end for
+#         return len
+#     end getLength()
+#
+# Inputs: 
+#   $a0 - string to get length of
+# Outputs:
+#   $v0 - length of string as integer
+# Registers modified: $sp, $ra
+getLength:
+        addi $sp, $sp, -4
+        sw $ra, 0($sp)
+        
+        move $t0, $a0 # First index of input string
+        li $t1, 0   # Input string length
+        lb $t2, edgeStringMaxLen
+
+        getLengthLoop:
+                lb $t3, ($t0)
+                bge $t1, $t2, getLengthEnd # If length == 3, exit
+                beqz $t3, getLengthEnd # If null terminated
+                seq $t3, $t3, 0x0A
+                bnez $t3, getLengthEnd # If line feed terminated
+                
+
+                add $t0, $t0, 1
+                addi $t1, $t1, 1
+                
+                j getLengthLoop
+
+        getLengthEnd:
+                move $v0, $t1
+                
+                lw $ra, 0($sp)
+                addi $sp, $sp, 4
+                
+                jr $ra
+
+                
+
+# Description: Splits the edge string into two indices for the array.
+#
+# Pseudo representation:
+#     private (int, int) splitInput(String $a0, int $a1):
+#         // EX: $a0 = "A11"
+#         int col = input[0] - 0x41 # 'A' becomes 0
+#         int row = input[1] - 0x30 # '1' becomes 1
+#         if (input.length == 2):
+#             return inputArr
+#         row = (row * 10) + (input[2] - 0x30) # Row becomes '11'
+#         --row # Row becomes '10'
+#         return (col, row)
+#     end splitInput()
+#
+# Inputs: 
+#   $a0 - edge string containing the selected edge to split
+#   $a1 - length of the edge string
+# Outputs:
+#   $v0 - the col index of the selected edge
+#   $v1 - the row index of the selected edge
+# Registers modified: $sp, $ra
 splitInput:
         addi $sp, $sp, -4
         sw $ra, 0($sp)
         
         lbu $t0, 0($a0)
-        subu $t0, $t0, 0x41
-        
         lbu $t1, 1($a0)
+        
+        subu $t0, $t0, 0x41
         subu $t1, $t1, 0x30
         
         seq $t2, $a1, 2
         bnez $t2, splitInputExit
         
-        lbu $t3, 2($a0)
-        subu $t3, $t3, 0x30
-        li $t4, 0xA
-        mult $t1, $t4
-        mflo $t1
-        addu $t1, $t1, $t3 
-
-splitInputExit:
-        lw $ra, 0($sp)
-        addi $sp, $sp, 4
+        splitInputTwoNums: # a0 will not be greater than 3 chars
+                lbu $t3, 2($a0)
+                subu $t3, $t3, 0x30
+                li $t4, 10
+                mulu $t1, $t1, $t4
+                addu $t1, $t1, $t3
         
-        move $v0, $t0
-        move $v1, $t1
-
-        jr $ra
-
-validateUserInput:
-        addi $sp, $sp -4
-        sw $ra, 0($sp)
+        splitInputExit:
+                addi $t1, $t1, -1
         
-        blt $a1, 0x41, invalidInput
+                move $v0, $t0
+                move $v1, $t1
+                
+                lw $ra, 0($sp)
+                addi $sp, $sp, 4
         
-        lw $ra, 0($sp)
-        addi $sp, $sp, 4
-        jr $ra
-        # Column accepted between 0x41 && 0x4F
-        
-        
-invalidInput:
-        la $a0, invalidInputWarning
-        li $v0, 4
-        syscall
-        
-        j getUserInput
+                jr $ra
 
 exit:
         li $v0, 10
         syscall
-#        jr $ra
