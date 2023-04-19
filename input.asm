f569a4c7421c18698c5745671d9e2505b7c3e0a2
Nick Donovan <114261268+nick-donovan@users.noreply.github.com>
2023-04-19 00:39:12 -0500

    feat(input.asm): adding user input validation with warnings
    In this commit:
     - Program now filters out invalid input validation from user and displays
       applicable warning
     - New methods include validate_input_range and validate input_length
     - New unused method get_caller_address which will return the label address
       of who called it (may not be needed depending on implementation)

--- input.asm
+++ input.asm
@@ -7,19 +7,32 @@
 #  input is valid, and displaying error messages if the input is invalid.
 
 .data
-        input_user_selected_edge: .space 3
+        input_user_selected_edge:      .space 3
         input_user_selected_edge_size: .byte 4
-        input_edge_string_max_len: .byte 3
-        input_user_prompt_string: .asciiz "Enter an edge to claim (ex: A2): "
-        input_invalid_input_warning: .asciiz "Please enter a letter (A-O) and a number (0-11)."
+        input_edge_string_max_len:     .byte 3
+        input_user_prompt_string:      .asciiz "Enter an edge to claim (ex: A2): "
+        input_invalid_input_warning:   .asciiz "Please enter a letter (A-O) and a number (0-11)."
         input_insf_characters_warning: .asciiz "PLACEHOLDER WARNING - NOT ENOUGH CHARS IN EDGE STRING"
+        
         .globl input_get_user_input
         .globl input_convert_edge_string
 
 .text
 main:
-
+	jal board_initialize_board
+	li $a0, 0
+	li $a1, 1
+	li $a2, 0
+	jal board_update_edge
+	jal board_print_board
         jal input_get_user_input
+        
+        move $a0, $v0
+	move $a1, $v1
+	li $a2, 0
+	jal board_update_edge
+	jal board_print_board
+
         j exit
 
 # Description: Prints the input_user_prompt_string and saves a selected edge into 
@@ -42,25 +55,25 @@ input_get_user_input:
         addi $sp, $sp -4
         sw $ra, 0($sp)
         
+        i_gui_begin:
+        
         la $a0, input_user_prompt_string
         jal print_string
         
         la $a0, input_user_selected_edge
-        lb $a1, input_user_selected_edge_size
+        lI $a1, 0X04
         li $v0, 8
         syscall
-        
+                
         jal input_convert_edge_string
-        
-        # todo: jal input_validate_user_input
-        
+
         lw $ra, 0($sp)
         addi $sp, $sp, 4
         
         jr $ra
         
-# Description: Converts an inputted edge string (Ex: "A2") into indicies (Ex: (0, 1)
-#
+# Description: Converts an inputted edge string (Ex: "A2") into indicies (Ex: (0, 1))
+# 
 # Pseudo representation:
 #     public (int col, int row) input_convert_edge_string(String a0):
 #         int v0 = input_get_length(a0)
@@ -70,26 +83,42 @@ input_get_user_input:
 #     end input_convert_edge_string()
 #
 # Inputs: 
-#   $a0 - string to get length of
+#   $a0 - the string to convert to indicies
 # Outputs:
-#   $v0 - length of string as integer
-# Registers modified: $sp, $ra
+#   $v0 - the col index of the selected edge
+#   $v1 - the row index of the selected edge
+# Registers modified: $sp, $ra, $s0 (restored), $s1 (restored)
 input_convert_edge_string:
-        addi $sp, $sp -4
+        addi $sp, $sp -12
+        sw $s0, 8($sp)
+        sw $s1, 4($sp)
         sw $ra, 0($sp)
-      
+  
+  	move $s0, $a0
+  
         jal input_get_length
-
-        # todo: add error handling for len < 2
+        move $s1, $v0
         
-        move $a1, $v0
+        move $a0, $s1
+        jal input_validate_input_length
+        
+        move $a0, $s0
+        move $a1, $s1
         jal input_split_input
-
-        # $v0 - the col index of the selected edge
-        # $v1 - the row index of the selected edge
-
+        move $s0, $v0
+        move $s1, $v1
+        
+        move $a0, $s0
+        move $a1, $s1
+        jal input_validate_input_range
+    
+    	move $v0, $s0
+    	move $v1, $s1
+    
         lw $ra, 0($sp)
-        addi $sp, $sp, 4
+        lw $s1, 4($sp)
+        lw $s0, 8($sp)
+        addi $sp, $sp, 12
         
         jr $ra
 
@@ -181,6 +210,8 @@ input_split_input:
                 li $t4, 10
                 mulu $t1, $t1, $t4
                 addu $t1, $t1, $t3
+                
+                jal print_break
         
         i_si_exit:
                 addi $t1, $t1, -1
@@ -192,6 +223,98 @@ input_split_input:
                 addi $sp, $sp, 4
         
                 jr $ra
+
+
+input_get_caller_address:
+	slt $t0, $a0, $a1
+	beq $t0, $zero, i_gca_swap_values
+
+	j i_gca_continue
+
+	i_gca_swap_values:
+		move $t0, $a0
+		move $a1, $a0
+		move $a0, $t0
+		
+	i_gca_continue:
+		slt $t0, $ra $a1
+		beq $t0, $zero, i_gca_second_label
+		
+		move $v0, $a0
+		
+		jr $ra
+		
+	i_gca_second_label:
+		move $v0, $a1
+		jr $ra
+# a0 length
+input_validate_input_length:
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	slti $t0, $a0, 2
+	bne $t0, $zero, i_vil_invalid
+	
+	j i_vil_valid	
+	
+	i_vil_invalid:
+		li $a0, 0x00
+		jal _warning_throw_warning
+		addi $sp, $sp, 16
+		j i_gui_begin
+		
+	i_vil_valid:
+		lw $ra, 0($sp)
+		addi $sp, $sp, 4
+		jr $ra
+
+#   $a0 - the col index of the selected edge
+#   $a1 - the row index of the selected edge
+input_validate_input_range:
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	i_vir_check_ranges:
+	li $t0, 0x01
+	slti $t1, $a0, 0 # if (a0 < 0) t1 = 1
+	bne $t1, $zero, i_vir_invalid # if t1 != 0 invalid
+	
+	sgt $t1, $a0, 16 # if (a0 > 16) t1 = 1
+	bne $t1, $zero, i_vir_invalid # if t1 !- 0 invalid
+	
+	li $t0, 0x02
+	slti $t1, $a1, 0
+	bne $t1, $zero, i_vir_invalid
+	
+	sgt $t1, $a1, 12
+	bne $t1, $zero, i_vir_invalid
+	
+	i_vir_check_edge:
+	li $t0, 0x03
+	andi $t1, $a0, 0x01
+	andi $t2, $a1, 0x01
+	
+	addu $t1, $t1, $t2
+	bne $t1, 1, i_vir_invalid
+	
+	i_vir_check_occupied:
+	jal board_is_edge_claimed #TAKESE AO AND A1
+	li $t0, 0x04
+	andi $t1, $v0, 0x01###########
+	beq $t1, $zero, i_vir_invalid
+	
+	j i_vir_valid
+	
+	i_vir_invalid:
+		move $a0, $t0
+		jal _warning_throw_warning
+		addi $sp, $sp, 16
+		j i_gui_begin
+		
+	i_vir_valid:
+		lw $ra, 0($sp)
+		addi $sp, $sp, 4
+		jr $ra
 
 exit:
         li $v0, 10
